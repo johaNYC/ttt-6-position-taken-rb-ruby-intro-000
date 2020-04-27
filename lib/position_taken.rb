6573252450cbd4c8f1e9080e0e74d23b29fbc9db
Johana Gutierrez <johagutierrezg@gmail.com>
2020-04-27 21:07:46 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,10 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
- if board == " " && index == " "
-   return
- elsif board == " " != index == " "
+ if(board [index] == " ") == (board [index] == " ")
+   return true
+ else
+   return false
 end
 
-end
 
-position_taken(board, index)
