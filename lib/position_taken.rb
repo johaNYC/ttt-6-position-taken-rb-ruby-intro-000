f9d0949688a18ad23cf3da077285a90a3f099fd9
Johana Gutierrez <johagutierrezg@gmail.com>
2020-04-27 21:09:03 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,13 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
- if(board [index] == " ") == (board [index] == " ")
-   return true
- else
+ if (board [index] == " ") || (board [index] == " ")
    return false
+ else
+   return true 
+   end
+   
 end
+
 
 
